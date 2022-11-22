ba3abcadacc76dced3c0729a3bc8e2749a291a18
chrystian <chrystian_sales@hotmail.com>
2022-11-21 21:42:27 -0300

    primeiro commit

---
+++ Projeto Carlos e Chrystian/js/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDS52H2tz1v_cCXWsG50arImXZji3nAK68",
+  authDomain: "proje-clinica.firebaseapp.com",
+  projectId: "proje-clinica",
+  storageBucket: "proje-clinica.appspot.com",
+  messagingSenderId: "776424617793",
+  appId: "1:776424617793:web:92259e44a6daff8d2853c0"
+};
+
+
+// Inicializando o Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
